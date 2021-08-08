c827bb76430e91f1984adbf312e40b01ce213709
rockstar-programmer <rockstarrprogrammerr@gmail.com>
2021-08-08 15:23:41 +0700

    lesson 1

--- Main.cpp
+++ Main.cpp
@@ -2,6 +2,36 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+void swapColor(bool &changed, int &color, GLFWwindow* window)
+{
+	double seconds_passed = glfwGetTime();
+	bool is_turn = (int)seconds_passed % 2 == 0;
+
+	if (is_turn)
+	{
+		if (!changed)
+		{
+			if (color == 0)
+			{
+				glClearColor(0.0f, 0.0f, 1.0f, 1.0f);
+				color = 1;
+			}
+			else if (color == 1)
+			{
+				glClearColor(1.0f, 0.65f, 0.0f, 1.0f);
+				color = 0;
+			}
+			glClear(GL_COLOR_BUFFER_BIT);
+			glfwSwapBuffers(window);
+			changed = true;
+		}
+	}
+	else
+	{
+		changed = false;
+	}
+}
+
 int main()
 {
 	glfwInit();
@@ -26,12 +56,15 @@ int main()
 
 	glViewport(0, 0, 800, 800);
 
-	glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
-	glClear(GL_COLOR_BUFFER_BIT);
-	glfwSwapBuffers(window);
+	//glClearColor(1.0f, 0.65f, 0.0f, 1.0f);
+	//glClear(GL_COLOR_BUFFER_BIT);
+	//glfwSwapBuffers(window);
 
+	bool changed = false;
+	int color = 0;
 	while (!glfwWindowShouldClose(window))
 	{
+		swapColor(changed, color, window);
 		glfwPollEvents();
 	}
 
